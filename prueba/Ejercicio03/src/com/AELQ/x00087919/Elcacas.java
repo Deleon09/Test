6a0bfcb559bcc8fa6db5cc2c98ea6f9ad50e2344
Deleon09 <alberto101481@gmail.com>
2020-04-18 21:40:35 -0600

    funciona

---
+++ prueba/Ejercicio03/src/com/AELQ/x00087919/Elcacas.java
@@ -0,0 +1,17 @@
+package com.AELQ.x00087919;
+
+public class Elcacas {
+    private String youtuber;
+
+    public Elcacas(String youtuber) {
+        this.youtuber = youtuber;
+    }
+
+    public String getYoutuber() {
+        return youtuber;
+    }
+
+    public void setYoutuber(String youtuber) {
+        this.youtuber = youtuber;
+    }
+}
